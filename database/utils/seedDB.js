e328aba36bd98ac1c39ed1d8dd6506b272695b6b
Eric Lin <eric.lin67@myhunter.cuny.edu>
2025-12-12 22:42:50 -0500

    Added pfp for pre existing campus & students

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -11,32 +11,37 @@ const seedDB = async () => {
 	const dummy_campus = await Campus.create({
 		name: "Hunter College",
 		address: "695 Park Ave, New York, NY 10065",
-		description: "This is a school in New York, New York."
+		description: "This is a school in New York, New York.",
+		imageUrl: "https://s29068.pcdn.co/wp-content/uploads/hunter-new-york-city-street-view.jpg.optimal.jpg"
 	});
 	// Create a new campus
 	const dummy_campus2 = await Campus.create({
 		name: "Queens College",
 		address: "65-30 Kissena Blvd, Queens, NY 11367",
-		description: "This is a school in Queens, New York."
+		description: "This is a school in Queens, New York.",
+		imageUrl: "https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/schools/queens-college/QC_campus.jpg"
 	});
 	// Create a new campus
 	const dummy_campus3 = await Campus.create({
 		name: "Brooklyn College",
 		address: "2900 Bedford Ave, Brooklyn, NY 11210",
-		description: "This is a school in Brooklyn, New York."
+		description: "This is a school in Brooklyn, New York.",
+		imageUrl: "https://www.usnews.com/dims4/USNEWS/cc98eaa/17177859217/resize/800x540%3E/quality/85/?url=https%3A%2F%2Fwww.usnews.com%2Fcmsmedia%2F1e%2Fef%2Fc09954f940139695bcf218fc9084%2F2s9a2340-l.jpg"
 	});
 	
 	// Create a new student for a campus
 	const dummy_student = await Student.create({
 		firstname: "Joe",
       	lastname: "Smith",
-		email: "joesmith@gmail.com" //added dummy email since it can't be null
+		email: "joesmith@gmail.com", //added dummy email since it can't be null
+		imageUrl: "https://img.favpng.com/24/25/10/kids-face-glasses-cartoon-boy-with-glasses-and-hoodie-in-cute-design-HzvE280x_t.jpg"
 	});
 	// Create a new student for a campus
 	const dummy_student2 = await Student.create({
 		firstname: "Mary",
       	lastname: "Johnson",
-		email: "maryjohnson@gmail.com" //added dummy email since it can't be null
+		email: "maryjohnson@gmail.com", //added dummy email since it can't be null
+		imageUrl: "https://t3.ftcdn.net/jpg/10/24/11/58/360_F_1024115848_VTfuHjHj9UVVvrUOaDQqm2clMspgRnGs.jpg"
 	});
 
 	// Add students to campuses
